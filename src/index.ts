6405992b433952c7d14b4c971d6931531c98474d
Alfred Young <alfred@codenaturally.com>
2020-02-26 14:43:53 -0800

    asks for exported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { attempt } from './attempt';
 import { ask } from './ask';
 import { asks } from './asks';
+import { asksFor } from './asks-for';
 import { empty } from './empty';
 import { fromFuture } from './from-future';
 import { fromNode } from './from-node';
@@ -16,6 +17,7 @@ export default {
     attempt,
     ask,
     asks,
+    asksFor,
     empty,
     fromFuture,
     fromNode,
